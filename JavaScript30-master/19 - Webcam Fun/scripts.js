e18ed38c7154435cf8dd512498c7ef6abea32d1c
Florent <florentparis28@gmail.com>
2021-03-06 00:55:03 +0100

    day 19

--- JavaScript30-master/19 - Webcam Fun/scripts.js
+++ JavaScript30-master/19 - Webcam Fun/scripts.js
@@ -24,12 +24,30 @@ function getVideo() {
 
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
+        const pixels = ctx.getImageData(0, 0, width, height);
+        pixels = redEffect(pixels);
+        ctx.putImageData(pixels, 0, 0);
     }, 16);
 }   
 
 function takePhoto() {
     snap.currentTime = 0;
     snap.play();
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.innerHTML = `<img src="${data}" alt="handsome man" />`;
+    strip.insertBefore(link, strip.firstChild);
+  }
+
+function redEffect(pixels){
+    for(let i=0; 0 < pixels.data.length; i+=4){
+        pixels.data[i+0] = pixels.data[i+0] + 200;
+        pixels.data[i+1] = pixels.data[i+1] - 50;
+        pixels.data[i+2] = pixels.data[i+2] * 0.5;
+    }
+    return pixels;
 }
 
 getVideo();
